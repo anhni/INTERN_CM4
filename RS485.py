6953a57454459d785e96c10e2f2c327f2195d3e2
Anh Ni <anhni220502002@gmail.com>
2023-06-28 11:03:09 +0700

    Ni 4

--- RS485.py
+++ RS485.py
@@ -44,8 +44,8 @@ print("portName:",portName)
 if portName != "None":
     ser = serial.Serial(port=portName, baudrate=9600)
 
-relay1_ON = [0, 6, 0, 0, 0, 255, 200, 91]
-relay1_OFF = [0, 6, 0, 0, 0, 0, 136, 27]
+relay1_ON = [1, 6, 0, 0, 0, 255, 201, 138]
+relay1_OFF = [1, 6, 0, 0, 0, 0, 137, 202]
 
 while True:
 #  ser.write(relay1_ON)                                                                                                         
