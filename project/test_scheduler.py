948ca6db1b50f108728e65cea06d064030c39560
jkcvkjc <94946495+jkcvkjc@users.noreply.github.com>
2023-07-29 00:35:17 +0700

    update

--- project/test_scheduler.py
+++ project/test_scheduler.py
@@ -7,15 +7,16 @@ scheduler.SCH_Init()
 
 task1 = Task1()
 task2 = Task2()
+task3 = Task1()
 
 # #normal test
 # scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 5000)
-# scheduler.SCH_Add_Task(task2.Task2_Run, 2000, 5000)
-# scheduler.SCH_Add_Task(task1.Task1_Run, 3000, 5000)
+# scheduler.SCH_Add_Task(task2.Task2_Run, 3000, 5000)
+# scheduler.SCH_Add_Task(task3.Task1_Run, 4000, 5000)
 #one shot test
 scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 5000)
-scheduler.SCH_Add_Task(task2.Task2_Run, 2000, 0)
-scheduler.SCH_Add_Task(task1.Task1_Run, 3000, 5000)
+scheduler.SCH_Add_Task(task2.Task2_Run, 3000, 0)
+scheduler.SCH_Add_Task(task3.Task1_Run, 4000, 5000)
 
 while True:
     scheduler.SCH_Update()
